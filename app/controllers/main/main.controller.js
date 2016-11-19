05023c65c441363735fedfea00e28ff4fc1b36f9
gena-rybin <genarybin1979@gmail.com>
2016-11-19 08:41:45 +0200

    if localstorage is full, load clients from it; if not - load from json

--- app/controllers/main/main.controller.js
+++ app/controllers/main/main.controller.js
@@ -2,15 +2,28 @@
 
 app.controller('phoneBookCtrl', function ($scope,
                                           $state,
-                                          loadFromJsonService) {
+                                          loadFromJsonService, localStorageService) {
 
   //  $scope.writeLS = localStorageService.write;
 
-    loadFromJsonService.getData().then(function (res) {
-        $scope.users = res;
-        console.log($scope.users);
-    });  //doesn't work
+    $scope.getLS = localStorageService.getObj();
 
+    // if localStorage is full - take data from it
+    // if localStorage is empty - take data from json
+    loadFromJsonService.getData().then(function (res) {
+        if ($scope.getLS) {
+            console.log('full ls');
+            $scope.users = $scope.getLS;
+            return;
+        }
+        //localStorageService.write(555);
+        console.log('empty ls');
+        $scope.users = res;
+        localStorageService.write(res);
+    });
+
+
+    $scope.clearLS = localStorageService.clear;
 
     $scope.functionGoToEditePage = functionGoToEditePage;
 
